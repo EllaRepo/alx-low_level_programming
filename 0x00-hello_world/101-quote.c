a79749ac5c88f31d06df3da9c82da6c219e0a6ed
Eliab E <eliaberango@gmail.com>
2023-02-16 03:51:12 -0800

    Update 101-quote file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 20i15-10-19\n";
-
-	write(2, msg, sizeof(msg) - 1);
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+		
+	write(2, msg, sizeof(msg));
 	return (1);
 }
