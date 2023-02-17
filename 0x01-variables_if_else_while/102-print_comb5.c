27db98c2ef8a6fb2a4d1767cb5afd24fad8e4eae
Eliab E <eliaberango@gmail.com>
2023-02-17 05:00:01 -0800

    Add 102-print_comb5.c file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible different combination of three
+ *        digits separated by ',' followed by a space
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 57; j++)
+		{
+			for (k = i; k < 58; k++)
+			{
+				for (l = j + 1; l < 58; l++)
+				{
+					putchar (i);
+					putchar (j);
+					putchar (' ');
+					putchar (k);
+					putchar (l);
+					if (i == 57 && j == 56)
+						break;
+					putchar (',');
+					putchar (' ');
+				}
+			}
+		}
+	}
+		putchar ('\n');
+	return (0);
+}
