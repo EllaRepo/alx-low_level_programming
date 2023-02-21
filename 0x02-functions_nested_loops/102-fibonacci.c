9ece2cf81e10a0aeebd2b5a2fdee8b704111cfda
Eliab E <eliaberango@gmail.com>
2023-02-21 12:09:49 -0800

    Add 102-fibonacci.c file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 50 Fibonacci numbers,
+ *        starting with '1' and '2', followed by a new line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	long int previous, current;
+
+	previous = 1;
+	current = 2;
+	printf("%ld, %ld, ", previous, current);
+
+	for (i = 0; i < 48; i++)
+	{
+		long int tmp;
+
+		tmp = previous + current;
+		printf("%ld", tmp);
+		if (i == 47)
+			break;
+		printf(", ");
+		previous = current;
+		current = tmp;
+	}
+	printf("\n");
+	return (1);
+}
