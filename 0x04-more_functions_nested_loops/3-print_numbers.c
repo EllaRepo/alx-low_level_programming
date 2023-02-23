e94044ae91a10117941a695852d343010664bd87
Eliab E <eliaberango@gmail.com>
2023-02-22 23:55:48 -0800

    Add 3-print_numbers.c and modified main.h

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers, from 0 to 9,
+ *                 followed by a new line
+ *
+ * Return: None.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
+
