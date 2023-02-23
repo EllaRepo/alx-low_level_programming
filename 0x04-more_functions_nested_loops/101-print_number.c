e4133881917f64a0b1106c03f167f4df48bfc2c7
Eliab E <eliaberango@gmail.com>
2023-02-23 12:48:38 -0800

    Modified 101-print_number.c file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,15 +8,28 @@
  */
 void print_number(int n)
 {
+	int i, j;
+
 
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
-
+	i = n;
+	j = 1;
+	while (i / 10)
+	{
+		j *= 10;
+		i /= 10;
+	}
+	i = n;
+	while (j > 1)
+	{
+		_putchar((i / j) + '0');
+		i = i - ((i / j) * j);
+		j /= 10;
+	}
+	_putchar((n % 10) + '0');
 }
 
