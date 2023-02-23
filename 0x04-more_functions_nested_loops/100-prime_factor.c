9aa98b597b3c3942209d7099fcc7567dda600bcf
Eliab E <eliaberango@gmail.com>
2023-02-23 11:50:46 -0800

    Add 100-prime_factors.c file

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Finds and prints the largest prime factor of the number 612852475143,
+ * followed by a new line.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int i, n, largest_pn;
+
+	n = 612852475143;
+        i = 3;
+        while (i <= n)
+        {
+                if (n % i == 0)
+                {
+                        if (i > largest_pn)
+                                largest_pn = i;
+                        n = n / i;
+                        i = 1;
+                }
+                i += 2;
+        }
+        printf("%ld\n", largest_pn);
+	return (0);
+}
+
