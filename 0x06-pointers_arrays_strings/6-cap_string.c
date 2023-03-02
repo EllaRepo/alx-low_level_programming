0b3e3c9e257b20b1832744ab9aa7c7b2d00e9b4e
Eliab E <eliaberango@gmail.com>
2023-03-02 01:18:55 -0800

    Modified 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,3 @@
-int isseparator(char c);
-
 /**
  * cap_string - Capitalizes all words of string
  * @s: A string
@@ -9,41 +7,24 @@ int isseparator(char c);
 char *cap_string(char *s)
 {
 	char *str;
+	char separators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"',
+		'(', ')', '{', '}', '\0'};
+	int i;
 
 	str = s;
 
 	while (*str != '\0')
 	{
-		if (isseparator(*str))
+		for (i = 0; separators[i] != '\0'; i++)
 		{
-			if (*(str + 1) >= 'a' && *(str + 1) <= 'z')
-				*(str + 1) -= 32;
+			if (separators[i] == *str)
+			{
+				if (*(str + 1) >= 'a' && *(str + 1) <= 'z')
+					*(str + 1) -= 32;
+			}
 		}
 		str++;
 	}
 
 	return (s);
-}
-
-/**
- * isseparator - Checks if a character is a separator
- * @c: The character
- *
- * Return: 1 if a character is a separator
- *         0 otherwise
- */
-int isseparator(char c)
-{
-	char separators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"',
-		'(', ')', '{', '}', '\0'};
-
-	int i;
-
-	for (i = 0; separators[i] != '\0'; i++)
-	{
-		if (separators[i] == c)
-			return (1);
-	}
-
-	return (0);
 }
