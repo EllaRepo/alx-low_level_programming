47436edd23b0139ef2757d8518375e21b43124f9
Eliab E <eliaberango@gmail.com>
2023-03-08 10:33:17 -0800

    Fixing error

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * is_unique - Checks the uniqueness of a character in a given string
  * @s: Point to a string
@@ -9,9 +10,12 @@
 int is_unique(char *s, char c)
 {
 	if (*s == '\0')
-		return (1);
-	if (*s == c)
 		return (0);
+	if (*s == c)
+	{
+		s++;
+		return (1 + is_unique(s, c));
+	}
 	s++;
 	return (is_unique(s, c));
 }
@@ -25,7 +29,8 @@ int is_unique(char *s, char c)
  */
 int wildcmp(char *s1, char *s2)
 {
-	char *tmp;
+	char *tmp1, *tmp2;
+	int c1, c2;
 
 	if ((*s1 == '\0' && *s2 == '\0') ||
 			(*s1 == '\0' && *s2 == 0x2A && *(s2 + 1) == '\0'))
@@ -37,19 +42,14 @@ int wildcmp(char *s1, char *s2)
 	}
 	if (*s2 == 0x2A && *(s2 + 1) == *s1)
 	{
-		tmp = s1 + 1;
-		if (is_unique(tmp, *s1))
+		tmp1 = s1 + 1, tmp2 = s2 + 2, c1 = c2 = 0;
+		c1 = is_unique(tmp1, *s1), c2 = is_unique(tmp2, *s1);
+		
+		if (c1 == c2 || c1 == 0)
 		{
 			s2++;
 			return (wildcmp(s1, s2));
 		}
-		tmp = s2 + 2;
-		if (!is_unique(tmp, *s1))
-		{
-			s2++;
-			return (wildcmp(s1, s2));
-		}
-
 	}
 	if (*s2 == '\0')
 		return (0);
