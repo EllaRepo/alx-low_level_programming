0cd735861cf58078d671d36301d2bf51557bdef4
Eliab E <eliaberango@gmail.com>
2023-03-10 03:03:06 -0800

    Add 4-add.c file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Adds positive numbers and prints the result
+ * @argc: The number of command line arguments
+ * @argv: An array of pointers pointing to the arguments
+ *
+ * Return: 0 if all arguments are valid
+ *         1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	argv++;
+	for (sum = 0; *argv != NULL; argv++)
+	{
+		if (atoi(*argv))
+		{
+			sum += atoi(*argv);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
