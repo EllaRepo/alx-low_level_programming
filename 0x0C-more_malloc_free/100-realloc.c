2d85d9340284001fe14ba4de7f9816a7e6a72142
Eliab E <eliaberango@gmail.com>
2023-03-16 08:49:18 -0700

    fixing error

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,10 +11,9 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
-		if (ptr != NULL)
-			free(ptr);
+		free(ptr);
 		return (NULL);
 	}
 	if (ptr == NULL)
