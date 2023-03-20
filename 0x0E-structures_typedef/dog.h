35e68c962282956b836b40a68de520931d6f2611
Eliab E <eliaberango@gmail.com>
2023-03-20 07:21:18 -0700

    Add a dog.h header including a structure for dog's information

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure for dog's information
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: This structure is for the basic information of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
