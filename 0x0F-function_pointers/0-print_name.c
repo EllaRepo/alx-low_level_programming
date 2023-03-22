8ec88ed3e8b455e474369b024870eb5a70c637f9
Eliab E <eliaberango@gmail.com>
2023-03-22 04:19:56 -0700

    Add null parameter check in 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 /**
  * print_name - Prints a name
  * @name: name string
@@ -7,5 +9,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
