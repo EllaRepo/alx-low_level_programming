86beb644a4a78861961a25a5a5ee3c1c118fae93
Eliab E <eliaberango@gmail.com>
2023-03-30 02:18:21 -0700

    Add 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * _free -  frees a list_t list except the head
+ * @head: pointer to the head of the list
+ *
+ * Return: None
+ */
+void _free(list_t *head)
+{
+	if (head->next)
+		_free(head->next);
+	free(head->str);
+	free(head->next);
+}
+/**
+ * free_list -  frees a list_t list
+ * @head: pointer to the head of the list
+ *
+ * Return: None
+ */
+void free_list(list_t *head)
+{
+	_free(head);
+	free(head);
+}
