7a57d4f350071f6a7b80e60e71de7c5ee9aa57c3
Eliab E <eliaberango@gmail.com>
2023-04-03 12:31:00 -0700

    Add a condition to check if the index of the node to be inserted is valid

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *node_ptr, *tmp;
 	unsigned int i = 0;
 
-	if (!head)
+	if (*head == NULL && idx != 0)
 		return (NULL);
 	tmp = malloc(sizeof(listint_t));
 	if (!tmp)
