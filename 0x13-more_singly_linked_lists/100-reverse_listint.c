f3588bb3c52638a40253a4db36b798afa7679df4
Eliab E <eliaberango@gmail.com>
2023-04-03 12:55:15 -0700

    Fix return error

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -13,23 +13,16 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *node_ptr1, *node_ptr2;
 
-	if (!head)
-		return (NULL);
-	if (!((*head)->next))
-		return (*head);
+	node_ptr1 = NULL;
+	node_ptr2 = NULL;
 
-	node_ptr2 = (*head)->next;
-	node_ptr1 = *head;
-	node_ptr1->next = NULL;
-	*head = node_ptr2;
-	node_ptr2 = node_ptr2->next;
-	(*head)->next = node_ptr1;
-	while (node_ptr2)
+	while (*head)
 	{
-		node_ptr1 = *head;
-		*head =  node_ptr2;
-		node_ptr2 = node_ptr2->next;
-		(*head)->next = node_ptr1;
+		node_ptr1 = (*head)->next;
+		(*head)->next = node_ptr2;
+		node_ptr2 = *head;
+		*head = node_ptr1;
 	}
+	*head = node_ptr2;
 	return (*head);
 }
