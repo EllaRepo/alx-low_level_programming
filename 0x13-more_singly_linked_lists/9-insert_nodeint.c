a7e47520c69a9a8fe8ee1978d7d425b8ef664eb9
Eliab E <eliaberango@gmail.com>
2023-04-04 12:20:59 -0700

    Refactored the code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,6 +2,23 @@
 #include "lists.h"
 
 /**
+ * create_node - creates a new node
+ * @n: integer
+ *
+ * Return: the address of the new node
+ *         NULL if it failed
+ */
+listint_t *create_node(int n)
+{
+	listint_t *node = malloc(sizeof(listint_t));
+
+	if (!node)
+		return (NULL);
+	node->n = n;
+	node->next = NULL;
+	return (node);
+}
+/**
  * insert_nodeint_at_index - inserts a new node at a given position
  * @head: pointer to the head of the list
  * @idx: the index of the list where the new node should be added
@@ -16,27 +33,35 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *node_ptr, *tmp;
 	unsigned int i = 0;
 
+	if (idx == 0 && !(*head))
+	{
+		*head = create_node(n);
+		return (*head);
+	}
+
 	node_ptr = *head;
-	if (idx > 0)
+	while (node_ptr)
 	{
-		for (; i < idx - 1 && node_ptr; i++)
-			node_ptr = node_ptr->next;
+		if (idx == 0)
+		{
+			tmp = create_node(n);
+			if (!tmp)
+				return (NULL);
+			tmp->next = *head;
+			*head = tmp;
+			return (*head);
+		}
+		else if (idx - 1 == i)
+		{
+			tmp = create_node(n);
+			if (!tmp)
+				return (NULL);
+			tmp->next = node_ptr->next;
+			node_ptr->next = tmp;
+			return (tmp);
+		}
+		node_ptr = node_ptr->next;
+		i++;
 	}
-	if (!head && idx != 0)
-		return (NULL);
-	tmp = malloc(sizeof(listint_t));
-	if (!tmp)
-		return (NULL);
-	tmp->n = n;
-	if (idx == 0)
-	{
-		tmp->next = *head;
-		*head = tmp;
-	}
-	else
-	{
-		tmp->next = node_ptr->next;
-		node_ptr->next = tmp;
-	}
-	return (node_ptr);
+	return (NULL);
 }
