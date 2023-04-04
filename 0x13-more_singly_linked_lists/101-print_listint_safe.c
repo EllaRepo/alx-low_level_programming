aaab09da014bf2810ced1398cfb546ac9d2f6ea5
Eliab E <eliaberango@gmail.com>
2023-04-04 10:30:27 -0700

    Refactored code to support different edge cases

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,6 +3,20 @@
 #include "lists.h"
 
 /**
+ * free_listint_ptr -  frees a listint_ptr_t list
+ * @head: pointer to the head of the list
+ *
+ * Return: None
+ */
+void free_listint_ptr(listint_ptr_t *head)
+{
+	if (head)
+	{
+		free_listint_ptr(head->next);
+		free(head);
+	}
+}
+/**
  * print_listint_safe - prints a listint_t linked list
  * @head: pointer to listint_t
  *
@@ -10,20 +24,36 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *prev = head;
+	listint_ptr_t *node_ptr, *tmp;
 	size_t i = 0;
 
+	node_ptr = NULL;
 	while (head != NULL)
 	{
-		if (prev < head)
+		tmp = node_ptr;
+		while (tmp)
 		{
-			printf("-> [%p] %i\n", (void *) head, head->n);
+			if ((void *) head == tmp->list_ptr)
+			{
+				printf("-> [%p] %i\n", (void *) head, head->n);
+				free_listint_ptr(tmp);
+				return (i);
+			}
+			tmp = tmp->next;
+		}
+		tmp = malloc(sizeof(listint_ptr_t));
+		if (!tmp)
+		{
+			free_listint_ptr(node_ptr);
 			exit(98);
 		}
-		printf("[%p] %i\n", (void *) head, head->n);
-		prev = head;
+		tmp->next = node_ptr;
+		tmp->list_ptr = (void *) head;
+		node_ptr = tmp;
+		printf("[%p] %i\n", tmp->list_ptr, head->n);
 		head = head->next;
 		i++;
 	}
+	free_listint_ptr(node_ptr);
 	return (i);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,7 +14,19 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
-
+/**
+ * struct listint_ptr - singly linked list
+ * @list_ptr: pointer to void
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_ptr
+{
+	void *list_ptr;
+	struct listint_ptr *next;
+} listint_ptr_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
