29bda61f3cbe4ecb31fdb8f46d1b207ab112bac8
Eliab E <eliaberango@gmail.com>
2023-04-10 12:37:16 -0700

    Fix error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	for (len = 0; filename && filename[len]; len++)
+	for (len = 0; text_content && text_content[len]; len++)
 		;
 	wr = write(fd, text_content, len);
 	close(fd);
