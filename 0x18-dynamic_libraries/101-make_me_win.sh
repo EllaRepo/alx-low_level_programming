009641b8c27cf566f07b1f2c55d99225860dad51
Eliab E <eliaberango@gmail.com>
2023-05-22 14:00:11 -0700

    Add 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-content=$(wget https://raw.githubusercontent.com/EllaRepo/alx-low_level_programming/master/0x18-dynamic_libraries/crand.so -q -O -)
-export LD_PRELOAD=/tmp/crand.so
+content=$(curl https://raw.githubusercontent.com/EllaRepo/alx-low_level_programming/master/0x18-dynamic_libraries/crand.so -q -O -)
+export LD_PRELOAD="$content"
