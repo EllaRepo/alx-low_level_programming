93b7cf0ce8fb37bd8d60ee93f88fbde579f16e9c
Eliab E <eliaberango@gmail.com>
2023-05-22 13:36:54 -0700

    Add 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/EllaRepo/alx-low_level_programming/blob/18e0a0b6d929c52b98c3c1352b983483afa3a24d/0x18-dynamic_libraries/librand.so
+wget -P /tmp/ https://raw.githubusercontent.com/EllaRepo/alx-low_level_programming/master/0x18-dynamic_libraries/librand.so
 export LD_PRELOAD=/tmp/librand.so
