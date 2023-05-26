9e398ea6b956e2d5629f41c4a0de046635afb79d
Eliab E <eliaberango@gmail.com>
2023-05-26 10:22:06 -0700

    Fix error

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-content=$(curl https://raw.githubusercontent.com/EllaRepo/alx-low_level_programming/master/0x18-dynamic_libraries/crand.so -q -O -)
-export LD_PRELOAD="$content"
+wget -P /tmp/ https://raw.githubusercontent.com/EllaRepo/alx-low_level_programming/master/0x18-dynamic_libraries/crand.so
+export LD_PRELOAD=/tmp/crand.so
