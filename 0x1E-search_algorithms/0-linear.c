8e3f19817bbb086f7cac47433427177d258045a3
EllaRepo <eliaberango@gmail.com>
2023-09-13 12:04:01 -0400

    Add 0-linear.c search_algos.h

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - prints all the elements of a list_t list
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ *
+ * Return: first index where value is located
+ *         -1 If value is not present in array or if array is NULL
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array || size == 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (array[0]);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef H_SEARCH_ALGOS_H
+#define H_SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include "search_algos.h"
+
+int linear_search(int *array, size_t size, int value);
+
+#endif 
