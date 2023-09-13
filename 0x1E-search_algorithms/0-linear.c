0c1bf0d47403d52a9d9478662de396dccfb44156
EllaRepo <eliaberango@gmail.com>
2023-09-13 12:19:47 -0400

    Fix betty style error

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,13 +13,14 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (!array || size == 0)
-		return (-1);
-	for (i = 0; i < size; i++)
+	if (array != NULL && size > 0)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (array[0]);
+		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+				return (array[0]);
+		}
 	}
 	return (-1);
 }
